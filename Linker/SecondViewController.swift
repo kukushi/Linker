6c2a99027c9df0741f15fb92d835ac3b2e0b2592
kukushi <nekop@outlook.com>
2014-12-18 17:45:04 +0800

    Initial Commit

---
+++ Linker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Linker
+//
+//  Created by kukushi on 12/18/14.
+//  Copyright (c) 2014 Xing He. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
