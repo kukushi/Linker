a7b2ecc3ed3cb398a4405439298a1d5062a9ed26
kukushi <nekop@outlook.com>
2014-12-19 15:55:35 +0800

    Initial Commit

---
+++ BangumiKit/BangumiKit/BangumiKit.h
@@ -0,0 +1,19 @@
+//
+//  BangumiKit.h
+//  BangumiKit
+//
+//  Created by kukushi on 12/19/14.
+//  Copyright (c) 2014 Xing He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BangumiKit.
+FOUNDATION_EXPORT double BangumiKitVersionNumber;
+
+//! Project version string for BangumiKit.
+FOUNDATION_EXPORT const unsigned char BangumiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BangumiKit/PublicHeader.h>
+
+
